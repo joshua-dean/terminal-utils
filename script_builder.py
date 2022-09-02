e13099270dab81850b4337b3481ea0dd6aaa1291
Joshua Dean <deanx252@umn.edu>
2022-09-01 23:11:44 -0500

    Added ability to configure templates and added template for EC2 instance commands.

--- script_builder.py
+++ script_builder.py
@@ -1,4 +1,5 @@
 """Build .ps1 file for use."""
+from powershell_utils.templates.ec2_ssh import get_ec2_ssh_fns
 
 if __name__ == "__main__":
     # loosely defined for now
@@ -18,14 +19,30 @@ if __name__ == "__main__":
                 }, 
             }
         ],
+        "templates": [ 
+            {
+                "str_out": get_ec2_ssh_fns("MyEC2Instance", "i-0c8f8f8f8f8f8f8f8"),
+                "aliases": [ 
+                    "get-my-ec2-ip",
+                    "start-my-ec2-instance",
+                    "stop-my-ec2-instance",
+                    "ssh-my-ec2-instance",
+                ]
+            }
+        ],
         "output": "./test.ps1",
     }
 
     with open(config["output"], "w") as output_fd:
         for command in config["commands"]:
-            with open(f"powershell-utils/{command['script_name']}", "r") as script_fd:
+            with open(f"powershell_utils/{command['script_name']}", "r") as script_fd:
                 output_fd.write(script_fd.read())
                 for alias, command in command["aliases"].items():
                     output_fd.write(f"\nSet-Alias -Name {alias} -Value {command}")
                 output_fd.write("\n\n")
+        for template in config["templates"]:
+            cmd_str, command_names = template["str_out"]
+            output_fd.write(cmd_str)
+            for alias in template["aliases"]:
+                output_fd.write(f"\nSet-Alias -Name {alias} -Value {command_names[template['aliases'].index(alias)]}")
     
