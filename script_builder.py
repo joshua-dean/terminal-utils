78402b7ed2d31dd98ac62180d8b963b092ecbf4e
Joshua Dean <joshua.dean@sentera.com>
2023-06-20 11:45:23 -0500

    Added basic module functionality (still very clunky).

--- script_builder.py
+++ script_builder.py
@@ -1,5 +1,7 @@
 """Build .ps1 file for use."""
 from powershell_utils.templates.ec2_ssh import get_ec2_ssh_fns
+from powershell_utils.modules.pwsh_settings import set_vi_bindings, set_intellisense_bindings
+from powershell_utils.modules.external_modules import posh_git
 
 if __name__ == "__main__":
     # loosely defined for now
@@ -36,12 +38,28 @@ if __name__ == "__main__":
                 ]
             }
         ],
+        "modules": [
+            {
+                "str_out": set_vi_bindings(),
+            },
+            {
+                "str_out": set_intellisense_bindings(
+                    "Shift+Tab"
+                )
+            },
+            {
+                "str_out": posh_git(),
+            }
+        ],
         "output": "./demo.ps1",
     }
 
     with open(config["output"], "w") as output_fd:
+        for module in config["modules"]:
+            cmd_str, command_names = module["str_out"]
+            output_fd.write(cmd_str)
         for command in config["commands"]:
-            with open(f"powershell_utils/{command['script_name']}", "r") as script_fd:
+            with open(f"powershell_utils/commands/{command['script_name']}", "r") as script_fd:
                 output_fd.write(script_fd.read())
                 for alias, command in command["aliases"].items():
                     output_fd.write(f"\nSet-Alias -Name {alias} -Value {command}")
